19fdfeb7aaf944bf2f01e0dd76330ccbdf435dae
LeviBehar <levib8603@gmail.com>
2026-04-12 06:35:37 +0300

    Initial commit: Lichess-inspired Racing Kings platform skeleton

---
+++ src/core/README.md
@@ -0,0 +1,7 @@
+# The Goal: RacingKings.org
+
+## about:
+Inspired by the **Lichess philosophy**, this project aims to create a dedicated, Free Forever, No Ads, and Open Source **home for the Racing Kings chess variant community.**
+
+## Status:
+This project is in its very early infancy. We are currently architecting the core bitboard engine and the FFI bridge. Expect bugs, expect frequent changes, but most importantly—expect speed.
---
+++ src/core/types.h
@@ -0,0 +1,37 @@
+// Copyright (C) 2026 Levi Behar
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdint.h>
+
+enum {
+    WHITE, BLACK, COLOR_NB = 2
+};
+
+enum PieceType {
+    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, PIECE_NB = 6
+};
+
+typedef uint64_t Bitboard;
+typedef uint8_t Color;
+typedef uint8_t Square;
+typedef uint8_t Piece;
+
+typedef struct {
+    Bitboard ByColorBB[COLOR_NB];
+    Bitboard ByTypeBB[PIECE_NB];
+    Color Side;
+} Board;
+
+#endif
