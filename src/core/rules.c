dec2f9899209b2f1bacd21ff044a34618935e161
levi <levib8603@gmail.com>
2026-05-05 20:32:40 +0300

    remove test function in core/rules.c

--- src/core/rules.c
+++ src/core/rules.c
@@ -11,55 +11,6 @@
 int six_bits = 0x3F;
 
 
-void print_bb(Bitboard bb, char *label) {
-    printf("-- %s --\n", label);
-    for (int rank = 7; rank >= 0; rank--) {
-        printf("%d  ", rank + 1);
-        for (int file = 0; file < 8; file++) {
-            int sq = rank * 8 + file;
-            if ((bb >> sq) & 1ULL)
-                printf("1 ");
-            else
-                printf(". ");
-        }
-        printf("\n");
-    }
-    printf("   a b c d e f g h\n\n");
-}
-
-int main(void) {
-    // הקצאת זיכרון ללוח ואיפוס (חשוב כדי למנוע Garbage values)
-    Board *board = calloc(1, sizeof(Board));
-    
-    // נגדיר OCC לדוגמה
-    board->Occ[WHITE] = (1ULL << 11); // d2
-    board->Occ[BLACK] = (1ULL << 43); // d6
-    
-    Bitboard OCC_ALL = (board->Occ[WHITE] | board->Occ[BLACK]);
-    
-    // שימוש במשתנה six_bits כדי לחלץ משבצת (למשל d4)
-    int move_example = 27; // d4
-    Square sq = move_example & six_bits;
-
-    // קריאה לפונקציות מה-DB
-    Bitboard rook_attacks = get_straight_attacks(sq, OCC_ALL);
-    Bitboard bishop_attacks = get_diagonal_attacks(sq, OCC_ALL);
-
-    printf("DEBUGGING CHESS CORE - LEVI\n");
-    printf("==========================\n\n");
-    
-    print_bb(OCC_ALL, "OCCUPANCY (d2, d6)");
-    print_bb(rook_attacks, "ROOK ATTACKS from d4");
-    print_bb(bishop_attacks, "BISHOP ATTACKS from d4");
-
-    // הדפסת הכתובת של הלוח (מה שניסית לעשות קודם)
-    printf("Board pointer address: %p\n", (void*)board);
-
-    free(board);
-    return 0;
-}
-
-
 int game_state(Board *board) {
     // TODO: Need to check If it's stalemate that also covers reverse stalemate's
     if (board->Pieces[BLACK][KING] & RANK_8) {
